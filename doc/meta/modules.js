aab64562ee4a2ad24d8a52bccd2773011a4d1b3e
弘树 <tiehang.lth@alibaba-inc.com>
2014-08-01 12:51:23 +0800

    sync doc

---
+++ doc/meta/modules.js
@@ -0,0 +1,3 @@
+config({
+    'kg/droplist/index': {requires: ['node','base']}
+});
---
+++ doc/mini.js
@@ -0,0 +1,12 @@
+/**
+ * @fileoverview 
+ * @author 阿克<ake.wgk@taobao.com>
+ * @module droplist
+ **/
+KISSY.add(function(S, Component) {
+
+  return Component;
+
+}, {
+  requires: ["./index"]
+});
